e2c7fcf2994c674ff973dbd6957eefc44facdd75
jsun5 <jsun5@stuy.edu>
2019-03-24 22:42:39 -0400

    works!

--- draw.py
+++ draw.py
@@ -31,6 +31,10 @@ def add_polygon( polygons, x0, y0, z0, x1, y1, z1, x2, y2, z2 ):
   lines connecting each points to create bounding triangles
   ===================="""
 def draw_polygons( polygons, screen, color ):
+    if len(polygons) < 3:
+        print("3 points please!")
+        pass
+		
     i = 0
     while i < len(polygons)-2:
         draw_line(polygons[i][0],polygons[i][1],polygons[i+1][0],polygons[i+1][1],screen,color)
